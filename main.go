b3f0926244779a066641e94f55ec039af8aefabd
nathakritbc602 <nathakritbc602@gmail.com>
2023-12-14 16:33:24 +0700

    update api

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ func hello(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"message": "world"})
 }
 
+func todos(ctx *gin.Context) {
+	ctx.JSONP(http.StatusOK, gin.H{"message": "todo lists"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/hello", hello)
+	router.GET("/todos", todos)
 	router.Run(":9090")
 }
