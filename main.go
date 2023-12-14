e46aef5526d500e88ef202a99e0770ba6a73ef1b
nathakritbc602 <nathakritbc602@gmail.com>
2023-12-14 16:04:23 +0700

    update

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func hello(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"message": "world"})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/hello", hello)
+	router.Run(":9090")
+}
